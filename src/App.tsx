f6536e99fdba31c9cc5851678a86b71dd7f62c9d
Tyler Weeres <tweeres04@gmail.com>
2020-12-23 14:39:54 -0800

    Firebase hosting; autoplay; game ending

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { range } from 'lodash';
+import { useState, useRef, useEffect, useCallback } from 'react';
+import { range, toNumber } from 'lodash';
 
 import './App.css';
 
@@ -6,12 +7,73 @@ import { useGame } from './useGame';
 import Card from './Card';
 
 export default function App() {
-	const { playedCards, playTurn, reset, trapCards, horses } = useGame();
+	const { playedCards, playTurn, reset, trapCards, horses, deck } = useGame();
+	const [isAutoplaying, setIsAutoplaying] = useState<NodeJS.Timeout | null>(
+		null
+	);
+	const intervalRef = useRef<HTMLInputElement>(null);
+
+	const stopAutoplaying = useCallback(
+		function stopAutoplaying() {
+			clearInterval(isAutoplaying as NodeJS.Timeout);
+			setIsAutoplaying(null);
+		},
+		[isAutoplaying]
+	);
+
+	useEffect(() => {
+		if (deck.length === 0) {
+			stopAutoplaying();
+		}
+	}, [deck.length, stopAutoplaying]);
+
+	function autoplay() {
+		if (isAutoplaying) {
+			stopAutoplaying();
+		} else {
+			setIsAutoplaying(
+				setInterval(playTurn, toNumber(intervalRef.current!.value))
+			);
+		}
+	}
+
+	const gameOver = horses.some((h) => h.position === 10) || deck.length === 0;
+
+	useEffect(() => {
+		if (gameOver) {
+			stopAutoplaying();
+		}
+	}, [gameOver, stopAutoplaying]);
 
 	return (
 		<div className="board">
-			<button onClick={playTurn}>play turn</button>
-			<button onClick={reset}>reset</button>
+			<button onClick={playTurn} disabled={!!isAutoplaying}>
+				play turn
+			</button>
+			<button
+				onClick={() => {
+					reset();
+					stopAutoplaying();
+				}}
+			>
+				reset
+			</button>
+			<input
+				type="number"
+				ref={intervalRef}
+				defaultValue="750"
+				disabled={!!isAutoplaying}
+			/>
+			<button onClick={autoplay}>{isAutoplaying ? 'stop' : 'autoplay'}</button>
+			{gameOver ? (
+				<span
+					style={{
+						fontSize: '2rem',
+					}}
+				>
+					Game over!
+				</span>
+			) : null}
 			<div style={{ display: 'flex' }}>
 				{range(0, 11).map((n) => (
 					<div className="column-header">{n}</div>
